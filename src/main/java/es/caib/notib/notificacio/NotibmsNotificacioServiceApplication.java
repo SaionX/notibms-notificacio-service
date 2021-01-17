943ceb99baf9eb8dd5c00d5c5666d99de1f9fe47
sion <sion.andreu@gmail.com>
2021-01-17 19:07:52 +0100

    Pujada inicial

---
+++ src/main/java/es/caib/notib/notificacio/NotibmsNotificacioServiceApplication.java
@@ -0,0 +1,13 @@
+package es.caib.notib.notificacio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotibmsNotificacioServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotibmsNotificacioServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/caib/notib/notificacio/NotibmsNotificacioServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package es.caib.notib.notificacio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotibmsNotificacioServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
